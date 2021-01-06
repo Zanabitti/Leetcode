488fa3acb2c0ecf47e213d99206f6d2a6c8ed35f
Zanabitti <krepper_9@hotmail.com>
2021-01-07 00:17:05 +0200

    d1

--- addllnumbers/linklist.ts
+++ addllnumbers/linklist.ts
@@ -7,10 +7,11 @@ class ListNode {
         this.next = (next===undefined ? null : next)
     }
 }
-
-
-
  
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
     return;
 };
+//l1 = [2,4,3], l2 = [5,6,4]
+let l1 = new ListNode(2,
+    new ListNode(4,
+        new ListNode(3)));
